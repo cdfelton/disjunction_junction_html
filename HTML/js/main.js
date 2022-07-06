83e6eed479fcd6c53283a315df587b0af021128c
fel-chen <felix.chen@gravity-ai.com>
2022-07-05 19:35:01 -0700

    Added slider recording data

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,6 +1,21 @@
-let tests = [['a',1],'b','c','d','e','f','g','h','i','j','k'];
+
+let tests = ["ID","Disjunct_A","Disjunct_B","Conjunction",
+  1,"John brought pizza to the party.","John brought pasta to the party.","John brought pizza and pasta to the party.",
+  2,"I’d like flowers.","I’d like champagne.","I’d like flowers and champagne.",
+  3,"If we hire Mary, everything will go well. ","If we hire Sue, everything will go well. ","If we hire Mary and Sue, everything will go well."];
 index = 0;
-length = 10;
+length = 3;
+userResult = [];
+practiceStarted = false;
+init();
+
+
+
+
+function init(){
+  tests = formatCSV(tests);
+  tests = shuffle(tests);
+}
 
 function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
@@ -20,6 +35,24 @@ function shuffle(array) {
   return array;
 }
 
+function formatCSV(list){
+  list.splice(0,4);
+  newList = [];
+  element = [];
+  counter = 0;
+  for(let i = 0; i <= list.length; i++){
+    if(counter == 4){
+      counter = 0;
+      newList.push(element);
+      element = [];
+    }
+    element.push(list[i]);
+    counter++;
+  }
+  console.log(newList);
+  return newList;
+}
+
 function goToConsent(){
   document.getElementById('startScreen').style='display:none';
   document.getElementById('consentForm').style='';
@@ -27,11 +60,10 @@ function goToConsent(){
 }
 function startPractice(){
   if(document.getElementById('consentCheck').checked){
-    console.log(tests);
     document.getElementById('consentForm').style='display:none';
     document.getElementById('practiceTestFrame').style='';
-    tests = shuffle(tests);
-    console.log(tests);
+    
+    practiceStarted = true;
   }
   else{
 // Show some error thing asking them to check consent form
@@ -43,7 +75,11 @@ function submitPractice(test, message) {
     return 0;
   }
   if(true){ // see if they have moved the slider
-    document.getElementById('practiceSentence').innerHTML = tests[index]; // clear plots in case one is already there
+    
+    document.getElementById('practiceRange').style='';
+    userResult.push(document.getElementById('practiceRange').value);
+    document.getElementById('practiceSentence').innerHTML = tests[index][1];
+    console.log(document.getElementById('practiceRange').value);
     index++;
   }
   else{ // ask them to move the slider
